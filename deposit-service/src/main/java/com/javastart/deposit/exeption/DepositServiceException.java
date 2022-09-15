b7e1aef2900ed9430a65c6289137ab05de41ba60
Vlad Danilevskiy <105595459+dan1chek@users.noreply.github.com>
2022-09-15 10:55:04 +0300

    Initial commit

---
+++ deposit-service/src/main/java/com/javastart/deposit/exeption/DepositServiceException.java
@@ -0,0 +1,9 @@
+package com.javastart.deposit.exeption;
+
+
+public class DepositServiceException extends RuntimeException {
+
+    public DepositServiceException(String message) {
+        super(message);
+    }
+}
